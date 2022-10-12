ef8b1a87266876c2c32605be8f8e9cf8172f6b51
root <root@LAPTOP-ROS4S4E6.localdomain>
2022-10-12 10:48:21 +0200

    Main update working get and fetch

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
-import './style.css'
+import './style.css';
 import { getWeatherFromLocation } from './weather-services';
 
-const weatherReport =document.querySelector<HTMLDivElement>('#weather-report');
+//const weatherReport =document.querySelector<HTMLDivElement>('#weather-report');
+let weatherTimestamp: number[] = [];
+let weatherTempreture: number[] = [];
+let weatherPrecipitationType: string[] = [];
+let weatherCloudCover: number[] = [];
+let weatherHumidity: number[] = [];
+let weatherWindDirection: string[] = [];
+let weatherWindSpeed: number[] = [];
 
-getWeatherFromLocation(20,20).then((response) => {
-    if (response.ok) {
-      response.json().then((jsonResponse) => {
-        for (let data of jsonResponse.results) {
-            console.log(data.init)
-        }
-      });
-    }
+getWeatherFromLocation(20, 20).then((response) => {
+  if (response.ok) {
+    response.json().then((jsonResponse) => {
+      for (let weatherData of jsonResponse.dataseries) {
+        weatherTimestamp.push(weatherData.timepoint);
+        weatherTempreture.push(weatherData.temp2m);
+        weatherPrecipitationType.push(weatherData.prec_type);
+        weatherCloudCover.push(weatherData.cloudcover);
+        weatherHumidity.push(weatherData.rh2m);
+        weatherWindDirection.push(weatherData.wind10m.direction);
+        weatherWindSpeed.push(weatherData.wind10m.speed);
+      }
+    });
+  }
 });
+
+console.log('test');
--- src/weather-services.ts
+++ src/weather-services.ts
@@ -1,10 +1,13 @@
-const BASE_URL = 'http://www.7timer.info/bin/api.pl?';
+const BASE_URL = 'https://www.7timer.info/bin/astro.php?';
 
-export function getWeatherFromLocation(lat: number,lon: number) {
-    return fetch(`${BASE_URL}lon=${lat}&lat=${lon}&product=astro&output=json`, {
-        headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           }
-    });
-  }
+export function getWeatherFromLocation(lat: number, lon: number) {
+  return fetch(
+    `${BASE_URL}lon=${lat}&lat=${lon}&ac=0&unit=metric&output=json&tzshift=0`
+    // , {
+    //     headers : {
+    //         'Content-Type': 'application/json',
+    //         'Accept': 'application/json'
+    //        }
+    // }
+  );
+}
